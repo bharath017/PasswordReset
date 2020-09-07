4f6abb116573bc9006f1ce465fb979f41af29d2a
bharathgowda538@gmail.com <bharathgowda538@gmail.com>
2020-09-07 22:26:41 +0530

    Updated form module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ResetComponent} from './reset/reset.component';
+import { ResetFormComponent } from './reset-form/reset-form.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path : '', component: ResetComponent},
+  {path: 'reset', component: ResetFormComponent}
+];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ResetComponent } from './reset/reset.component';
+import { bootstrap } from 'bootstrap';
+import { ResetService }from './reset/reset.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ResetFormComponent } from './reset-form/reset-form.component';
+import { ResetFormService } from './reset-form/reset-form.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResetComponent,
+    ResetFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [ResetService,ResetFormService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
